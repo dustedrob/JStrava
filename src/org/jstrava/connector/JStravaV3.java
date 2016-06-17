de9299d0d872ab27aa9a4ecc62b1f7a64fe0804f
Justin Robbins | hybris <justin.robbins@hybris.com>
2016-06-17 14:03:54 -0600

    added missing import

--- src/org/jstrava/connector/JStravaV3.java
+++ src/org/jstrava/connector/JStravaV3.java
@@ -14,6 +14,7 @@ import org.jstrava.entities.stream.Stream;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
