9bde13bc3758214b1856a9f093aaa00962e3fa6a
Mathias De Loore <mathiasdeloore@gmail.com>
2019-10-15 23:26:50 +0200

    Add methods to store and refresh the refresh token as advised by Strava

--- src/main/java/org/jstrava/authenticator/AuthResponse.java
+++ src/main/java/org/jstrava/authenticator/AuthResponse.java
@@ -5,10 +5,18 @@ import org.jstrava.entities.Athlete;
 
 public class AuthResponse {
     String access_token;
+    String refresh_token;
+    Long expires_at;
     Athlete athlete;
 
     public String getAccess_token() { return access_token; }
     public void setAccess_token(String token) { this.access_token = token; }
+
+    public String getRefresh_token() { return refresh_token; }
+    public void setRefresh_token(String token) { this.refresh_token = token; }
+
+    public Long getExpires_at() { return expires_at; }
+    public void setExpires_at(Long exp) { this.expires_at = exp; }
 
     public Athlete getAthlete() { return athlete; }
     public void setAthlete() { this.athlete = athlete; }
